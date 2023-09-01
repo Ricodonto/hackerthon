70c669a36e6dc66bd4c8e7e686ce5b8b1245adce
Nathan-44-rugo <131403788+Nathan-44-rugo@users.noreply.github.com>
2023-09-01 19:16:53 +0300

    initiate commit

--- ibookDB.py
+++ ibookDB.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
-#Load the datasetinto a dataframe
+# Load the dataset into a DataFrame
 df = pd.read_csv("books.csv")
 
-#Set and keep the required columns
+# Select and keep only the desired columns
 df = df[['title', 'authors', 'num_pages', 'isbn']]
 
-#Rename the columns to match your specified names
-df = df.rename(columns={'title':'Title','authors':'Authors','num_pages':'Number of pages','isbn':'ISBN'})
+# Rename the columns to match your specified names
+df = df.rename(columns={'title': 'Title', 'authors': 'Author', 'num_pages': 'Number of Pages', 'isbn': 'ISBN'})
 
-df.to_csv("ibookDB.csv",index=False)
+# Save the cleaned dataset to a new CSV file
+df.to_csv("cleaned_books.csv", index=False)
