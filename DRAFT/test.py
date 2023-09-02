82b2e7d9e3f42a57cea39206a3897dfcf1d69d92
Ricodonto <rico.njoroge@njuguna.com>
2023-09-02 22:21:47 +0300

    fake info

--- DRAFT/test.py
+++ DRAFT/test.py
@@ -1,18 +1,12 @@
 import openai
 
-def ai(prompt):
-    check = True
-    while check == True:
-        message = []
-        if len(message) == 0:
-            check = False
-            ###  REMOVE THE API KEY FROM HERE!
-            openai.api_key = "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"
-
-            # Make the gpt ask the user questions on what on they want
-            completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}])
-            message.append(completion.choices[0].message.content)
-    return message[0]
-
+def ai(p):
+    pp = [p]
+    message = [{"title":"Eloquent JavaScript: A Modern Introduction to Programming", "author":"Marijn Haverbeke", "isbn":"9781593275846", "rating":"4.5/5" },
+     {"title":"Eloquent JavaScript: A Modern Introduction to Programming", "author":"Marijn Haverbeke", "isbn":"9781593275846", "rating":"4.5/5" },
+     {"title":"Eloquent JavaScript: A Modern Introduction to Programming", "author":"Marijn Haverbeke", "isbn":"9781593275846", "rating":"4.5/5" },
+     ]
+    
+    return message
 if __name__ == '__main__':
     print(ai("how are you?"))
