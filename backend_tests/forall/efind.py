52424ef6b1a8f103b83cc7bf49f618a6b3834d2b
Ricodonto <rico.njoroge@njuguna.com>
2023-09-02 16:23:37 +0300

    forall

--- backend_tests/forall/efind.py
+++ backend_tests/forall/efind.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template,redirect, request
 
 #"forms" here is the name of the signup class python file, For the signup form, used to create the signup form
 from forms import Promptform
+from test import ai
+
 efind = Blueprint(__name__, 'efind')
 
 # @efind.route('/')
@@ -52,7 +54,12 @@ def landingpage():
     #Create a form object
     form = Promptform()
     if form.is_submitted():
+        #This is a regular dictionary in python
         result = request.form
+        # Remove the submit key
+        # Put the prompt value into the ai funciton
+        # Add a key called response with the value being the response  = ai(result['prompt'])
+        # put the response into the below render template funciton
         return render_template('response.html', result=result)
 
     return render_template('landingpage.html', form=form)
