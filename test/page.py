24fb794ced018cf5af2cf9cb0ffe7089d15991a8
Ricodonto <rico.njoroge@njuguna.com>
2023-09-03 03:42:02 +0300

    added html files

--- test/page.py
+++ test/page.py
@@ -8,5 +8,21 @@ app.config['SECRET_KEY'] = "test"
 def page():
     return render_template("page.html")
 
+@app.route("/base")
+def base():
+    return render_template("base_template.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/landing")
+def landing():
+    return render_template("landing_page.html")
+
+@app.route("/response")
+def response():
+    return render_template("response.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
