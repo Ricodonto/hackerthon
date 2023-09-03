5dfed90bf5543e9803fa935589da962fe3d560f6
Ricodonto <rico.njoroge@njuguna.com>
2023-09-03 05:06:39 +0300

    removing ai and putting fake data

--- test/chatgpt.py
+++ test/chatgpt.py
@@ -1,24 +1,12 @@
 import os
 from dotenv import load_dotenv
 load_dotenv('.env')
-
-import openai
-
 def ai(prompt):
-    check = True
-    while check == True:
-        message = []
-        if len(message) == 0:
-            check = False
-            openai.api_key = os.getenv("OPENAI_API_KEY")
-
-            completion = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                # Figure out whether you wanna give the assistant a role
-                messages=[
-                    {"role": "user", "content": prompt}])
-            message.append(completion.choices[0].message.content)
-    return message[0]
+    m = prompt 
+    messages = [{"title":"SQL Cookbook", "Author":"Anthony Molinaro","isbn":"0596009763", "reviews":"4.4/5", "description":"This book offers a collection of practical recipes for solving various SQL problems and challenges. It covers a wide range of topics, including database design, querying data, optimizing performance, and troubleshooting common issues"},
+                {"title":"SQL Cookbook", "Author":"Anthony Molinaro","isbn":"0596009763", "reviews":"4.4/5", "description":"This book offers a collection of practical recipes for solving various SQL problems and challenges. It covers a wide range of topics, including database design, querying data, optimizing performance, and troubleshooting common issues"},
+                {"title":"SQL Cookbook", "Author":"Anthony Molinaro","isbn":"0596009763", "reviews":"4.4/5", "description":"This book offers a collection of practical recipes for solving various SQL problems and challenges. It covers a wide range of topics, including database design, querying data, optimizing performance, and troubleshooting common issues"}]
+    return messages
 
 if __name__ == '__main__':
     print(ai("how are you?"))
