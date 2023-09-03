21e640bcbea60569ee66aea35aff471aee779b84
Ricodonto <rico.njoroge@njuguna.com>
2023-09-03 03:31:51 +0300

    html

--- test/page.py
+++ test/page.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app.config['SECRET_KEY'] = "test"
 
 @app.route("/")
 def page():
-    return "Hello World"
+    return render_template("page.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
