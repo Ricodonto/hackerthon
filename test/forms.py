0ca4a7081afeb656af6a1054f350c6fd4d00ba80
chegechris <128447894+chegechris@users.noreply.github.com>
2023-10-07 20:14:46 +0300

    centralized the hyperlinks and added it to the footer too

--- test/forms.py
+++ test/forms.py
@@ -3,5 +3,5 @@ from wtforms import StringField, SubmitField
 
 class PromptForm(FlaskForm):
     prompt = StringField("For example: What books should I read if I want to learn javascript")
-    submit = SubmitField("ENTER")
+    submit = SubmitField("Search")
 
