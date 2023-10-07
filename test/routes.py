65486cad51c026ca354afd25d460e6b7e2f3d51d
Roman Njoroge <roman.njoroge@njuguna.com>
2023-10-08 02:09:43 +0300

    feat: Middle of implementing login

--- test/routes.py
+++ test/routes.py
@@ -1,8 +1,10 @@
 import json
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect
 from forms import PromptForm
 from chatgpt import ai, cleanup, clear_history_file
 from flask import jsonify
+import os
+from supabase_py import create_client, Client
 
 
 
@@ -35,6 +37,37 @@ def landing():
 
     return render_template("landing_page.html", form=form)
 
+# Route for login
+@routes.route("/login", methods=['GET', 'POST'])
+def login():
+    error = False
+    # If it is a get request to this route display the login page
+    if request.method == "GET":
+        return render_template('login.html')
+    else:
+        # Get user details
+        username = request.form['username']
+        password = request.form['password']
+
+        # Fetch user with username
+        # Create connection to supabase
+        url = os.environ.get("SUPABASE_URL")
+        key = os.environ.get("SUPABASE_KEY")
+        supabase = create_client(url, key)
+
+        # select password of user with username
+        response = supabase.table('Users').select('hashed_password').eq('username', username)
+        if (response.count <= 0):
+            # Set error to true
+            error = True
+
+        # Compare hashed passwords
+        
+        
+        # Send JWT 
+
+        # Redirect to home page
+        return redirect("/")
 
 @routes.route("/about")
 def about():
