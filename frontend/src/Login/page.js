e217969a3ba39db1ed7adc37026797f3a1c0e8c5
Roman Njoroge <roman.njoroge@njuguna.com>
2023-10-25 22:53:22 +0300

    feat: Designing login and signup pages

--- frontend/src/Login/page.js
+++ frontend/src/Login/page.js
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
+import './styles.css';
+
 export default function Login() {
     return (
         <main>
-            <h2>Login</h2>
+            <h2>Sign into your Bookfinder account</h2>
+            <form className="login-form">
+                <div className="email">
+                    <label for="email-text-input" className="form-label">Email Address</label>
+                    <input required type="text" id="email-text-input" name="username" placeholder="Username" className="form-input" />
+                </div>
+                <div className="password">
+                    <label for="password-input" className="form-label">Password</label>
+                    <input required type="password" id="password-input" name="password" placeholder="Password" className="form-input" />
+                </div>
+                <input className="login-button" type="submit" value="Login" />
+            </form>
+            <div style={{textAlign: "center"}}>
+                <div className="redirect-to-login">
+                    <p style={{display: "inline"}}>Don't Have An Account, </p>
+                    <Link to="/signup" className="inline-link">Sign Up</Link>
+                </div>
+            </div>
         </main>
     );
 }
--- frontend/src/SignUp/page.js
+++ frontend/src/SignUp/page.js
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
+import '../Login/styles.css';
+
 export default function SignUp() {
     return (
         <main>
-            <h2>Sign up</h2>
+            <h2>Register to your Bookfinder account</h2>
+            <form className="login-form">
+                <div className="email">
+                    <label for="email-text-input" className="form-label">Email Address</label>
+                    <input required type="text" id="email-text-input" name="username" placeholder="Username" className="form-input" />
+                </div>
+                <div className="password">
+                    <label for="password-input" className="form-label">Password</label>
+                    <input required type="password" id="password-input" name="password" placeholder="Password" className="form-input" />
+                </div>
+                <input class="login-button" className="login-button" type="submit" value="Register" />
+            </form>
+            <div style={{textAlign: "center"}}>
+                <div className="redirect-to-login">
+                    <p style={{display: "inline"}}>Already Have An Account, </p>
+                    <Link to="/login" class="inline-link">Log In</Link>
+                </div>
+            </div>
         </main>
     );
 }
