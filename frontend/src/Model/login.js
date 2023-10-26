3e5714baa31660cd52b1965be006760f6ad602fa
Roman Njoroge <roman.njoroge@njuguna.com>
2023-10-26 11:57:57 +0300

    feat: Creating function for logging in and creating constants

---
+++ frontend/src/Model/login.js
@@ -0,0 +1,28 @@
+import { url } from "./constants";
+
+export async function sendLoginDetails(username, password) {
+    try {   
+        let formdata = new FormData();
+        formdata.append('username', username);
+        formdata.append('password', password);
+        let response = await fetch(
+            `${url}/login`,
+            {
+                method: 'POST',
+                body: formdata
+            }
+        );
+        console.log("Fetch Completed!")
+        let json = await response.json();
+        if (response.status !== 200) {
+            console.log(json);
+            throw new Error("Could Not Log In")
+        }
+    } catch(err) {
+        console.log(err);
+        throw new Error("Could Not Log In");
+    }
+    return new Promise((res, rej) => {
+
+    })
+}
