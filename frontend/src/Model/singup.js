ce9cf3a16c66d5b0b5c23ea9d324a5c63e9d43e4
Roman Njoroge <roman.njoroge@njuguna.com>
2023-10-26 12:18:57 +0300

    feat: Connecting signup to backend and implementing error handling

---
+++ frontend/src/Model/singup.js
@@ -0,0 +1,25 @@
+import { url } from "./constants";
+
+export async function signup(username, password) {
+    try {   
+        let formdata = new FormData();
+        formdata.append('username', username);
+        formdata.append('password', password);
+        let response = await fetch(
+            `${url}/signup`,
+            {
+                method: 'POST',
+                body: formdata
+            }
+        );
+        console.log("Fetch Completed!")
+        let json = await response.json();
+        if (response.status !== 200) {
+            console.log(json);
+            throw new Error("Could Not Register Account")
+        }
+    } catch(err) {
+        console.log(err);
+        throw new Error("Could Not Register Account");
+    }
+}
