caf7a8866194bafe81c90e297f51e87cd97a173b
Roman Njoroge <roman.njoroge@njuguna.com>
2023-10-28 21:13:09 +0300

    feat: Designing Settings Page

--- frontend/src/Settings/page.js
+++ frontend/src/Settings/page.js
@@ -1,52 +1,75 @@
 import { MyButton } from "../Home/page";
+import './styles.css';
 
 export default function Settings() {
     return (
-        <>
+        <div className="settings-page">
             {/* Heading */}
-            <h2>Manage your account</h2>
+            <h2 className="settings-h2">Manage your account</h2>
             {/* Username */}
-            <section>
-                <form>
-                    <div>
-                        <h3>Username</h3>
-                        <button type="submit">Change</button>
-                    </div>
-                    <input 
-                        placeholder="New Username"
-                        type="text"
-                        required
-                    />
-                </form>
-            </section>
-            {/* Password */}
-            <section>
-                <form>
-                    <div>
-                        <h3>Password</h3>
-                        <button type="submit">Change</button>
-                    </div>
-                    <input 
-                        placeholder="Old Password"
-                        type="password"
-                        required
-                    />
-                    <input 
-                        placeholder="New Password"
-                        type="password"
-                        required
-                    />
-                    <input 
-                        placeholder="Confirm Password"
-                        type="password"
-                        required
-                    />
-                </form>
-            </section>
-            <MyButton 
-                text={"Delete Account"} 
+            <div style={{backgroundColor: "#D9D9D9"}}>
+                <section>
+                    <form className="settings-form">
+                        <div className="settings-form-header">
+                            <h3>Username</h3>
+                            <button className='linkbox' type="submit">Change</button>
+                        </div>
+                        <div className="settings-input-block">
+                            <label for="new-username">New Username</label>
+                            <input
+                                id="new-username"
+                                placeholder="New Username"
+                                type="text"
+                                className="settings-input"
+                                required
+                            />
+                        </div>
+                    </form>
+                </section>
+                {/* Password */}
+                <section>
+                    <form className="settings-form">
+                        <div className="settings-form-header">
+                            <h3>Password</h3>
+                            <button className='linkbox' type="submit">Change</button>
+                        </div>
+                        <div className="settings-input-block">
+                            <label for="old-password">Old Password</label>
+                            <input
+                                id="old-password"
+                                placeholder="Old Password"
+                                type="password"
+                                className="settings-input"
+                                required
+                            />
+                        </div>
+                        <div className="settings-input-block">
+                            <label for="new-password">New Password</label>
+                            <input
+                                id="new-password"
+                                placeholder="New Password"
+                                type="password"
+                                className="settings-input"
+                                required
+                            />
+                        </div>
+                        <div className="settings-input-block">
+                            <label for="confirm-password">Confirm Password</label>
+                            <input
+                                id="confirm-password"
+                                placeholder="Confirm Password"
+                                type="password"
+                                className="settings-input"
+                                required
+                            />
+                        </div>
+                    </form>
+                </section>
+            </div>
+            <MyButton
+                text={"Delete Account"}
                 handleClick={() => console.log("Account Deleted")}
             />
-        </>
+        </div>
     );
 }
