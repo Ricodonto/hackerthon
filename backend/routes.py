4944ab7527bf8bf6828cb0c678df7de27957adf0
Ricodonto <rico.njoroge@njuguna.com>
2023-10-30 19:04:46 +0300

    expected reciever as a string

--- backend/routes.py
+++ backend/routes.py
@@ -714,7 +714,7 @@ def bad_feedback():
 @routes.route('/emailing', methods=['POST'])
 def emailing():
     # receiver is an email
-    receiver: str = request.form['reciever']
+    receiver: str = request.form['reciever'] # This is  a string, if many they are separated by commas
     prompt_asked: str = request.form['prompt_asked'] # is a string
     responses: str = request.form['responses'] # is a list
     
